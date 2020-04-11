6aff6ee74e5d0a8e32bc21f87919982fe8c6ec87
Trang68 <55749300+Trang68@users.noreply.github.com>
2020-04-11 14:52:47 +0300

    Add files via upload

---
+++ Point.java
@@ -0,0 +1,8 @@
+//package ru.spbstu.main.shapes;
+//
+//public interface Point {
+//	public static final float x = 0;
+//	public static final float y = 0;
+//    float getX();
+//    float getY();
+//}
